ee83d46ea5c8e7506931a003597ace8c5ff6bacc
lisab <lisabowles4797@outlook.com>
2018-03-29 14:23:59 -0400

    math_functions2.rb

---
+++ addition.rb
@@ -0,0 +1,5 @@
+def add(num1, num2)
+	total= num1 + num2
+	puts total
+end
+add(10,12)
--- math_functions2.rb
+++ math_functions2.rb
@@ -1,20 +1,24 @@
 def add(num1, num2)
-	puts num1 + num2
-end
-
-def multiply(num1, num2)
-	puts num1 * num2
+	total= num1 + num2
+	puts total
 end
 
 def subtract(num1, num2)
-	puts num1 - num2
+	total= num1 - num2
+	puts total
+end
+
+def multiply(num1, num2)
+	total= num1 * num2
+	puts total
 end
 
 def divide(num1, num2)
-	puts num1 / num2
+	total= num1 / num2
+	puts total
 end
 
 add(12,24)
-multiply(6,7)
-subtract(53,22)
-divide(42,7)
+subtract(53,33)
+multiply(3,5)
+divide(6,7)
