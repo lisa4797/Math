548fce4b81f343742232293a13f712d42e41c30a
lisab <lisabowles4797@outlook.com>
2018-03-29 14:51:00 -0400

    math_functions2.rb

--- math_functions2.rb
+++ math_functions2.rb
@@ -18,7 +18,19 @@ def divide(num1, num2)
 	puts total
 end
 
-add(12,24)
-subtract(53,33)
-multiply(3,5)
-divide(6,7)
+puts "Would you like to add, subtract, multiply, or divide?"
+answer = gets.chomp
+puts "What is the first number?"
+num1 = gets.chomp.to_i
+puts "What is your second number?"
+num2 = gets.chomp.to_i
+
+if answer == "add"
+	add(num1, num2)
+elsif answer == "subtract"
+	subtract(num1, num2)
+elsif answer == "multiply"
+	multiply(num1, num2)
+elsif answer == "divide"
+	divide(num1, num2)
+end
