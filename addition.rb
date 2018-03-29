cd2c144280dfb04694f1bbbe975e37dc51d1cdb6
lisab <lisabowles4797@outlook.com>
2018-03-29 14:11:48 -0400

    math_functions2.rb

--- addition.rb
+++
@@ -1,4 +0,0 @@
-def add(num1, num2)
-	puts num1 + num2
-end
-add(220 ,175)
---
+++ math_functions2.rb
@@ -0,0 +1,20 @@
+def add(num1, num2)
+	puts num1 + num2
+end
+
+def multiply(num1, num2)
+	puts num1 * num2
+end
+
+def subtract(num1, num2)
+	puts num1 - num2
+end
+
+def divide(num1, num2)
+	puts num1 / num2
+end
+
+add(12,24)
+multiply(6,7)
+subtract(53,22)
+divide(42,7)
